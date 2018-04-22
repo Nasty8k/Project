cd95eb33ece93963e2dea0fb30e89db1ec655556
drenchedj <kozorog_a@mail.ru>
2018-04-22 16:01:22 +0600

    On english

--- ListPlayers.c
+++ ListPlayers.c
@@ -3,20 +3,20 @@
 
 int main()
 {
-    char Player[15]; 
+    char Player[15];
     int i;
     FILE *file = fopen("ListPlayers.txt", "a+");
 
     if (file!=NULL)
-        printf("Удалось открыть файл для чтения и дополнения\n");
-    else printf("Не удалось открыть файл");
+        printf("Successfully it was possible to open a file for reading and additions\n");
+    else printf("Couldn't open file");
 
     printf("Please, enter a name: \n");
     scanf("%s", Player);
 
-    for(i=0; i<15; i++) {
+     for(i=0; i<15; i++) {
         if ((Player[i]=='#') || (Player[i]=='/'))
-        printf ("Имя не должно содержать знаков # и /!\n");
+        printf ("The name can`t contain # and /!\n");
     }
 
     fprintf(file, "%s", Player );
