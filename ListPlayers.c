6b60eeac5dee47820dd8eb91dd1ff582ab25b845
drenchedj <kozorog_a@mail.ru>
2018-04-22 16:56:18 +0600

    Add numeration

--- ListPlayers.c
+++ ListPlayers.c
@@ -4,8 +4,8 @@
 int main()
 {
     char Player[15];
-    int i;
-    FILE *file = fopen("ListPlayers.txt", "a+");
+    int i, j, n = 1;
+    FILE *file = fopen("ListPlayers.txt", "a+"); //
 
     if (file!=NULL)
         printf("Successfully it was possible to open a file for reading and additions\n");
@@ -14,11 +14,18 @@ int main()
     printf("Please, enter a name: \n");
     scanf("%s", Player);
 
-     for(i=0; i<15; i++) {
+    do {
+        j = fgetc(file);
+        if (j == '\n')
+            n++;}
+         while (j != EOF);
+
+    for(i=0; i<15; i++) {
         if ((Player[i]=='#') || (Player[i]=='/'))
         printf ("The name can`t contain # and /!\n");
     }
 
-    fprintf(file, "%s", Player );
+    fprintf(file, "%d. %s\n", n, Player);
     fclose(file);
 }
+
