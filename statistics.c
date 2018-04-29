cde39290ac527b02a7c0894af17b2ce659ceb188
Коля Таронтино <Kolya@localhost.localdomain>
2018-04-30 01:16:18 +0600

    Final version of statistics.c

--- statistics.c
+++ statistics.c
@@ -3,18 +3,38 @@
 #include <string.h>
 #include "statistics.h"
 
-
-void own_statistics(FILE *name)
+void scopy(char *s1)//добавляет в ".txt"
 {
-     char  string[20][40];
-     char c;
-     printf("YOUR STATISTICS\n");
-     
-    for ( int i = 0; i < 20; i++){
-        fscanf(name,"%s",string[i]); 
-        if ((c = fgetc(name)) != EOF){
+    char s2[] = ".txt";
+    strcat(s1,s2);
+}
+
+int counter(char *file_name)// считает '\n'
+{
+    FILE *file;
+    int i = 0;
+    char c;
+    file = fopen(file_name, "r");
+    while ((c = fgetc(file)) != EOF){
+        if (c == '\n')
+            i ++;
+    }
+    fclose(file);
+    return i;
+}
+    
+void own_statistics(char *name)
+{
+    FILE *namef;
+    char  string[20][40];
+    scopy(name);
+    printf("YOUR STATISTICS\n");
+    
+    namef = fopen(name,"r");
+    
+    for ( int i = 0; i <= counter(name); i++){
+        fscanf(namef,"%s",string[i]); 
         fprintf(stdout,"%s\n",string[i]);
-        }
     }
     
    
@@ -24,31 +44,47 @@ void general_statistics()
 {
 
     FILE *listf;
-    //FILE *name;
+    FILE *namef;
     char c;
     printf("GENERAL STATISTICS\n");
     
-     
-    listf = fopen("test.txt", "r");
+    char *list_pointer;
     int i = 0;
-    while ((c = fgetc(listf)) != EOF){
-        if (c == '\n')
-            i ++;
-    }
-    fprintf(stdout,"%d\n",i);
-    fclose(listf);
+    char list_string[] = {"test.txt"};
+    list_pointer = &list_string;
+    i = counter(list_pointer);
     
-    listf = fopen("test.txt", "r");
-    char list[i][18];
+    listf = fopen("ListPlayers.txt", "r");
+    char list[i][19];
+    char players[i][16];
     int num;
 
     for ( int j = 0; j < i; j++){
         fscanf(listf,"%d%c%s",&num,&c,list[j]); 
-        if ((c = fgetc(listf)) != EOF){
-        fprintf(stdout,"%s\n",list[j]);
-        }
+        strcpy(players[j],list[j]);
     }
     
+    
+    for ( int j = 0; j < i; j++){
+       scopy(list[j]);
+    }
+    
+    for(int g = 0; g < i; g ++){
+        char *name_pointer;
+        name_pointer = &list[g];
+        namef = fopen(name_pointer,"r");
+        char name[counter(name_pointer)][32];
+        fprintf(stdout,"%s|",players[g]);
+        for( int p =0; p <= counter(name_pointer); p++){
+            fscanf(namef,"%s",name[p]);
+            if (p == (counter(name_pointer)) - 1)
+            fprintf(stdout,"%s\n",name[p]);
+        }
+        
+        fclose(namef);
+    }
 
+    
+    
     fclose(listf);
 }
