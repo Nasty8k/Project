4109375bfb79c7295d0889b89fd442a0090868b0
Коля Таронтино <Kolya@localhost.localdomain>
2018-04-29 12:13:43 +0600

    First version of statistics.c/h

---
+++ statistics.c
@@ -0,0 +1,24 @@
+#include "stdio.h"
+
+
+void own_statistics(FILE *name)
+{
+     char string1[15];
+     char string2[15];
+     
+     while(fscanf(name, %s,string1)  != EOF){
+         fscanf(name,%s, string1);
+         if (string1 != EOF){
+                 string2 = string1;
+         }
+
+      printf("%s\n", string2);
+
+}
+
+void general_statistics()
+{
+
+   FILE *list;
+
+}
---
+++ statistics.h
@@ -0,0 +1,11 @@
+#indef _STATIS_
+#define _STATIS_
+
+
+void own_statistics(FILE *name);
+void general_statistics();
+
+
+
+
+#endif
