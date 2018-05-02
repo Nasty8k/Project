5ed471204c1e75d74041f22627a5f07a802703d9
Коля Таронтино <Kolya@localhost.localdomain>
2018-05-02 23:48:56 +0600

    4 version

--- training.c
+++ training.c
@@ -2,45 +2,58 @@
 #include "training.h"
 
 int check;
-void compare_string(char *s1,char *s2, struct progress *prog){
+int exit_print()
+{
+    char s;
+    printf("Уверены что хотите выйти?\n"
+            " YES(y) or NO(n)\n");	
+	scanf("%c", &s);
+	printf("%c\n",s);
+	
+	if( s == 'y')
+	   return 2;
+	 
+	 return 0;
+}
 	
 
+void compare_string(char *s1,char *s2, struct progress *prog){ // сравнивает данную и введенную строку
+	
 	
 	for (int len = 0; len < (strlen(s1)); len++){
 		if (s1[len] == s2[len]){
 			if(s1[len] != ' '){
-		        prog->points = prog-> points +1;
-		        prog-> right_s = prog-> right_s +1;
-		    }
+				prog-> right_s = prog-> right_s +1;
+				if (s1[len] != '-'){
+		            prog->points = prog-> points +1;
+			    }else prog->points = prog-> points +2;
+			}
 		}else prog-> wrong_s = prog-> wrong_s +1;
 	}
-	
-	fprintf(stdout,"%d\t%d\t%d\n",prog-> points, prog-> right_s, prog-> wrong_s );
-	
-	
-	
+		
+	fprintf(stdout,"%d\t%d\t%d\n",prog-> points, prog-> right_s,prog-> wrong_s );
 }	
+
 int write_string(char *string, struct progress *prog){
 	
 	char s2[69];
 	check = 0;
-	fprintf(stdout, "%s\n", string);
-	
 	printf("Начинайте ввод\n");
+	printf("Если хотите выйти нажмите # + Enter\n ");
+	printf("\n");
+	fprintf(stdout, "%s\n", string);
 	
 	int i =0;
 	do{
-		scanf("%c",&s2[i]);	
-		if (s2[i] == '#')
-			return -1;
+	    scanf("%c",&s2[i]);	
+	    if (s2[i] == '#')
+           return -1;
+	   
 		i++;
-	}while(s2[i-1] != '\n');
-	s2[i-1] = '\0';
-
+	    }while(s2[i-1] != '\n');
+	    s2[i-1] = '\0';
+	   
     compare_string(string,s2, prog);
-  //  fprintf(stdout, "%s\n", string);
-    
-    
     
     return 0;
 }
@@ -54,12 +67,11 @@ int training(int n_string, struct User *player)
 	struct progress prog;
 	prog.wrong_s = -1;
 	prog.right_s= 0;
-	prog.accur = 0;
 	prog.points = 0;
 	struct progress *p_prog;
 	p_prog =&prog;
 	
-	file = fopen("test.txt", "r");
+	file = fopen("GAME.txt", "r");
 	level = (player -> level)-1;
 	
 	for (int i = 0; i < (level + n_string); i++){
@@ -75,12 +87,12 @@ int training(int n_string, struct User *player)
 		    if (check == -1)
 		    	return -1;
     }
-    
-			
+    fclose(file);
+    printf("Отлично\n");
+	player -> level = level + n_string + 1;
+	player -> points = player-> points + prog.points;
+    player -> accur = MAX / player -> points;
 	
-return 0;	
 	
-	
-
-	
+    return 0;	
 }
