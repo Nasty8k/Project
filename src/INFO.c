a9def74f14ae416d5fc7a092ad716a2c8242535c
is742s08 <is742s08@localhost.localdomain>
2018-06-04 00:57:25 +0700

    Add INFO to develop

---
+++ src/INFO.c
@@ -0,0 +1,14 @@
+#include "data_global.h"
+#include "game_opening.h"
+/*После заполнения информацией для пользователя INFO будет включен в 5 пункт меню*/
+void information(void)
+{
+    opening();
+    printf("WELCOME!\n");
+    printf("[1. PLAY] is ..\n");
+    printf("[2. QUICK GAME] is ..\n");
+    printf("[3. SETTINGS] is ..\n");
+    printf("[4. TABLE] is ..\n");
+    printf("[5. INFO] is here\n");
+    
+}
