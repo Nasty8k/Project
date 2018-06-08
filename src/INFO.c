469f30fa29a0bd361d4355a9639e92b91396c8ad
Nasty8k <36221835+Nasty8k@users.noreply.github.com>
2018-06-08 22:12:47 +0700

    Add some info

--- src/INFO.c
+++ src/INFO.c
@@ -5,10 +5,9 @@ void information(void)
 {
     opening();
     printf("WELCOME!\n");
-    printf("[1. PLAY] is ..\n");
-    printf("[2. QUICK GAME] is ..\n");
-    printf("[3. SETTINGS] is ..\n");
-    printf("[4. TABLE] is ..\n");
+    printf("[1. PLAY] is the main of game process\n");
+    printf("[2. QUICK GAME] is just for fun\n");
+    printf("[3. SETTINGS] You can change TIME and STRINGS (from 1 to 99)\n");
+    printf("[4. TABLE] statistic of game and users\n");
     printf("[5. INFO] is here\n");
-    
 }
