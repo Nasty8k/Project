4fe8ef897aaa8e7a531fb5cb5d116eb56db76d96
is742s08 <is742s08@localhost.localdomain>
2018-06-21 00:40:46 +0700

    New version. Need develop

--- ListPlayers.c
+++ ListPlayers.c
@@ -2,43 +2,65 @@
  Нужна проверка и дороботка.
  1) Существует ли имя в списке
  2) Создание файла <NAME>.txt игрока в data/Users
- 3) Варианты выхода с успехом (1) или ошибкой (-1) 
+ 3) Варианты выхода с успехом (1) или ошибкой (-1)
+   проверку на латинские буквы
+   проверку списка
+   построение выбора меню
 */
-#include "ListPlayers.h"
-int main() {
-    
-    struct User * player = &PLAYER;
-    choice_name(player);
-    return 0;
-}
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int choice_name(struct User * player)
+int check_in_name(char *new_name, int all)
 {
-    FILE *origin = fopen("data/Game/ListPlayers.txt", "r");
-   
-    char *t, tmp_file[20] = {0}, gamers[10][15] = {0};
-    int input, i = 1, number = 0;
-    t = malloc(15 * sizeof(char));
-    while (fscanf(origin, "%d. %s\n", &number, gamers[i++]) != EOF) {
-        fprintf(stdout,"%d. %s\n", number, gamers[number]); 
+    int len = strlen(new_name);
+    if ((all >= 15) || (all == 0)) {
+        printf("The lenght of name from 1 to 15\n");
+        return 0;
     }
-    fprintf(stdout,"%d. SET NEW\n", ++number); 
-    printf("Enter your number:  ");
-    scanf("%d", &input);
-    if (input == number) {
-        printf("Pease, input your name (len < 14; without # and / )\n");
-        scanf("%15s", t);
-        if(strlen(t) == 0) printf("ERRRO\n");
-        printf(" tmp strlen %ld -%s-\n", strlen(t), t);
-        
-        printf("In new gamers -%s-\n", gamers[number]);
-    } else {
-        strcpy(tmp_file, gamers[input]);
-        printf("In tmp -%s-\n", tmp_file);
-    }
-        
-    fclose(origin);
-    
-    return 0;
+    if ((*new_name >= '0') && (*new_name <= '9')) {
+        printf("the first letter must be a symbol\n");
+        return 0;
+    }   
+    for(int i = 0; i < len; i++)
+        if ((new_name[i] == ' ')
+            || (new_name[i] == '#')
+               || (new_name[i] == '\\')) {
+            printf("Do not use space or <\\> and <#>\n");
+            return 0;
+        }
+    return 1;
 }
 
+int check_list(int num)
+{
+    printf("LIST \n");
+    
+    return num;
+}
+
+int main(){
+    char s[16] = {0};
+    char *p = s, c;
+    int i = 0, in = 0, f = 1;
+
+    printf("Enter Your num or New name: ");
+    while((c = getc(stdin))!='\n')
+    {   
+        if (i < 15) {
+            s[i] = c;
+            (c >= '0' && c <= '9') ? (f *=f) : (f = 0);
+        }
+        i++;
+    }
+    in = atoi(p);
+
+    (in * f == 0) ? (in = check_in_name(p, i)) : (in = check_list(in));
+    printf("IN = %d\n", in);
+    
+    printf("Yor choose: ");
+    scanf("%s", p);
+    printf("--> [%s]", p);
+    return 0;
+
+}
