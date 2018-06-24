4713dd0cfaab2c500b091063bae5b8c1f52fec14
is742s08 <is742s08@localhost.localdomain>
2018-06-25 02:17:39 +0700

    Prepare to merge

--- razminka1.c
+++ razminka1.c
@@ -2,58 +2,26 @@
 #include <stdlib.h>
 #include <time.h>
 
-int razminka(int timer){
-
-    int mist = 0, correct = 0;
+int razminka(int timer)
+{
+    float mist = 0.0, correct = 0.0;
     char vvod, ch;
-    float accuracy; // Точность
-    long int s_time1, s_time2, total = 0.0; //s_time2 - переменная для сохранения конца интервала
+    float accuracy;
+    long int s_time1, s_time2, total = 0.0;
 
-    s_time1 = time (NULL); //Считываем текущее системное время
-    s_time2 = time (NULL);
-
-    while (total < timer){
+while (total < timer) {
         s_time1 = time (NULL);
         ch = ('A' + rand()%('z'-'A'));
         printf("%c --> ", ch);
-
-            while((vvod = getc(stdin)) != '\n') {
-                if(ch -= vvod)
-                mist++;
-                else correct++;
-            }
-
-            s_time2 = time (NULL);
-            total += difftime (s_time2, s_time1); // Расчитываем разницу между двумя замерами времени с помощью функции difftime*
+        while ((vvod = getc(stdin)) != '\n') {
+            (ch -= vvod) ? mist++ : correct++;
+        }
+        s_time2 = time (NULL);
+        total += difftime(s_time2, s_time1);
     }
-
-    float zn; // Знаменатель
-    zn = mist + correct;
-    accuracy = (correct/zn);
-
-    printf("Mistakes: %d\n", mist); 
-    printf("Correct: %d\n", correct);
+    accuracy = (correct/(mist + correct));
+    printf("Mistakes: %.0f\n", mist); 
+    printf("Correct: %.0f\n", correct);
     printf("Accuracy: %.2f%%\n", accuracy*100);
-    printf("\nPress enter to continue...\n");
-go:
-    getchar();
-    printf("\nDo you want to go to the menu or go again?\n");
-    printf("Press # if you want to go to the menu or ! to go through again: \n");
-
-    char a;
-    scanf("%c", &a);
-
-    switch(a) {
-        case '#':
-            return 0;
-            break;
-        case '!':
-            main();
-            break;
-        default:
-            printf("\nTry it again\n");
-            goto go;
-            break;
-    }
 }
 
